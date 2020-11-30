67f8c609d47ec00b33c43df23097a673b01bf0ad
Nieco1812 <69344644+Nieco1812@users.noreply.github.com>
2020-11-30 10:21:12 +0700

    SPamKtL.py

--- SPamKtL.py
+++ SPamKtL.py
@@ -10,19 +10,7 @@ class Main:
 	def menu(self):
 		print("""
                      
-          def loginSC():
-	os.system('clear')
-	print"\033[1;97mSilahkan login SC nya dulu bosque\n"
-	username = raw_input("\033[1;96m[*] \033[1;97mUsername \033[1;91m: \033[1;92m")
-	password = raw_input("\033[1;96m[*] \033[1;97mPassword \033[1;91m: \033[1;92m")
-	if username =="D34TOR" and password =="Ganteng":
-		print"\033[1;96m[✓] \033[1;92mLogin success"
-		time.sleep(1)
-		login()
-	else:
-		print"\033[1;96m[!] \033[1;91mSalah!!"
-		time.sleep(1)
-                LoginSC()
+          "JANGAN LUPA KELEN SABREK BAR GAMING"
            
 
 		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
